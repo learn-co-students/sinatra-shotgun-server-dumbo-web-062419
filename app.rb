adebff1cf7655a8ab9608480613e1f502d730764
github cjl248 <github cjl248@cornell.edu>
2019-08-04 22:21:17 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "<h1><i> Welcome to your app!!!! </i></h1><br>
+    <p>Started my server using Shotgun!</p>"
   end
 
 end
